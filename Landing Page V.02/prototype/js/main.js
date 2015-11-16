bc4b184835ce91201c1f6dad38ef3fe7ce75cc6c
Briandito Priambodo <briandito.p@gmail.com>
2015-11-17 00:07:47 +0700

    navbar complete

--- Landing Page V.02/prototype/js/main.js
+++ Landing Page V.02/prototype/js/main.js
@@ -3,14 +3,14 @@ $(document).ready(function(){
     $('#burger').click(function(){
         $('#burger').toggleClass("active");
         
-        if($(".nav-group").hasClass("active")){
-         $(".nav-group").slideUp(500);    
-         $(".nav-group").removeClass("active");
+        if($(".navigation-menu").hasClass("active")){
+         $(".navigation-menu").slideUp(500);    
+         $(".navigation-menu").removeClass("active");
        }
        
         else{
-         $(".nav-group").addClass("active");
-         $(".nav-group").slideDown(500);
+         $(".navigation-menu").addClass("active");
+         $(".navigation-menu").slideDown(500);
        }
     });
 
