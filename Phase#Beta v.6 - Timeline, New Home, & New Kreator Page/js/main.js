5c2eb73f11e35fefdbf5ffe4a22a1bc43a06ef40
Briandito Priambodo <briandito.p@gmail.com>
2016-03-16 18:10:35 +0700

    new UI design

--- Phase#Beta v.6 - Timeline, New Home, & New Kreator Page/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){   
-    
-    $('#video_title h5 a').click(function(){
-        $('#video_desc__arrow').toggleClass("active");
-        $('#video_desc').toggleClass("ellipsis-2line");
-    });
-    
-});
