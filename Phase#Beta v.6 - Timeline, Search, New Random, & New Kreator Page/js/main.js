dbf29a2c8e21a61e689ed9d419b26d5a40da595b
Briandito Priambodo <briandito.p@gmail.com>
2016-03-20 01:46:56 +0700

    Update navigation bar, 90% progress

--- Phase#Beta v.6 - Timeline, Search, New Random, & New Kreator Page/js/main.js
+++ Phase#Beta v.6 - Timeline, Search, New Random, & New Kreator Page/js/main.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){   
     
-    $('#video_title h5 a').click(function(){
-        $('#video_desc__arrow').toggleClass("active");
-        $('#video_desc').toggleClass("ellipsis-2line");
+    
+    //burger navigation
+    $(document).ready(function(){
+        $('#nav-burger').click(function(){
+            $(this).toggleClass('open');
+        });
     });
     
 });
